2449acdd0e10074df9d30bdc39010caba76a7744
unknown <johncruzag@gmail.com>
2023-02-21 20:27:37 -0500

    changed database name

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://johnc:nodeproject@nodeexpressproject.ht5p2up.mongodb.net/?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://johnc:nodeproject@nodeexpressproject.ht5p2up.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
