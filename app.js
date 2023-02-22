d29d9b1c6dda1c9de787beb42999894f2bb3d41d
unknown <johncruzag@gmail.com>
2023-02-21 21:35:04 -0500

    third commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
+const connectDB = require('./db/connect');
+require('dotenv').config();
 //middleware
+
 app.use(express.json());
 
 //routes
@@ -13,5 +15,18 @@ app.get('/hello', (req,res) => {
 app.use('/api/v1/tasks',tasks)
 
 const port = 3000;
+/**
+ * We use async since we returned a promise on the connect.js file
+ * We will try{} and invoke connectDB using the .env file and if so we will listen to the port
+ * if there is an error we will console.log the error
+ */
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Connection to DB is successful...Listening to port... ${port}`));
+    } catch(error){
+        console.log(error)
+    }
+}
 
-app.listen(port, console.log(`Listening to port... ${port}`));
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,27 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://johnc:nodeproject@nodeexpressproject.ht5p2up.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+/**
+ * 
+ * This was done to utilize the .env file. We are creating a function here
+ * to be able to only invoke it if the connection of the db is successful
+ * and only then we will spin up the server.
+ * 
+ * mongoose.connect() method returns a Promise because it performs an asynchronous operation, which is establishing a connection to a MongoDB database.
+ * Connecting to a database involves network communication and can take some time,
+ * so it is not an instantaneous operation.
+ * To avoid blocking the event loop and to make the connection process more efficient,
+ * the mongoose.connect() method uses asynchronous I/O operations and returns a Promise that represents the outcome of the connection process.
+
+
+ */
+const connectDB = (url) => {
+    mongoose
+    .connect(url,{
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true ,})
+}
+
+module.exports = connectDB
